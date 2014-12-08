8ba9de15132ba584ac17a0e30e421a1832534310
Brandon Bloom <brandon@brandonbloom.name>
2014-12-07 23:38:16 -0500

    lein new patcomb

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to patcomb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/patcomb/core_test.clj
@@ -0,0 +1,7 @@
+(ns patcomb.core-test
+  (:require [clojure.test :refer :all]
+            [patcomb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
