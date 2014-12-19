dc185996e6d1d819fa9c0d15945761a6a07529b2
Brandon Bloom <brandon@brandonbloom.name>
2014-12-19 15:20:35 -0500

    Symbolic with vectors

--- src/patcomb/core.clj
+++ src/patcomb/core.clj
@@ -1,53 +1,61 @@
 (ns patcomb.core)
 
-(defprotocol Matcher
-  (-match [this env subject]))
+(defmulti -match (fn [form env subject]
+                   (if (seq? form)
+                     (first form)
+                     (class form))))
 
-(defn const [x]
-  (reify Matcher
-    (-match [_ env subject]
-      (assert env)
-      (when (= x subject)
-        env))))
+(defmethod -match 'const
+  [[_ x] env subject]
+  (assert env)
+  (when (= x subject)
+    env))
 
-(defn blank [sym]
+(defmethod -match 'blank
+  [[_ sym] env subject]
+  (assert env)
   (assert (symbol? sym))
-  (reify Matcher
-    (-match [_ env subject]
-      (assert env)
-      (if-let [x (env sym)]
-        (when (= x subject)
-          env)
-        (assoc env sym subject)))))
+  (if-let [x (env sym)]
+    (when (= x subject)
+      env)
+    (assoc env sym subject)))
 
-(defn as [sym matcher]
+(defmethod -match 'as
+  [[_ sym subpat] env subject]
   (assert (symbol? sym))
-  (assert (satisfies? Matcher matcher))
-  (reify Matcher
-    (-match [_ env subject]
-      (assert env)
-      (when-let [env* (-match matcher env subject)]
-        (assoc env* sym subject)))))
+  (assert env)
+  (when-let [env* (-match subpat env subject)]
+    (assoc env* sym subject)))
 
-(defn pattern [matcher]
-  (assert (satisfies? Matcher matcher))
-  (reify Matcher
-    (-match [_ env subject]
-      (if env
-        (and (-match matcher {} subject) {})
-        (-match matcher {} subject)))))
+(defmethod -match java.lang.Long
+  [value env subject]
+  (-match (list 'const value) env subject))
+
+(defmethod -match clojure.lang.PersistentVector
+  [subpats env subject]
+  (assert env)
+  (when (and (vector? subject) (= (count subpats) (count subject)))
+    (reduce (fn [env [pattern subject]]
+              (-match pattern env subject))
+            env
+            (map vector subpats subject))))
+
+;TODO some kind of generic traversal/zip thing for matching composites
+;;TODO -compile
 
 (defn match [pattern subject]
-  (-match pattern nil subject))
+  (-match pattern {} subject))
 
 (comment
 
   (require '[clojure.test :refer [is]])
 
-  (is (= (match (pattern (const 5)) 5) {}))
-  (is (= (match (pattern (const 5)) 0) nil))
-  (is (= (match (pattern (blank 'x)) 5) {'x 5}))
-  (is (= (match (pattern (pattern (blank 'x))) 5) {}))
-  (is (= (match (pattern (as 'x (const 1))) 1) {'x 1}))
+  (is (= (match '(const 5) 5) {}))
+  (is (= (match '(const 5) 0) nil))
+  (is (= (match '(blank x) 5) {'x 5}))
+  (is (= (match '(as x (const 1)) 1) {'x 1}))
+  (is (= (match '[] []) {}))
+  (is (= (match '[(blank x) 10] [5 10]) {'x 5}))
+  (is (= (match '[(blank x) 10] [5 11]) nil))
 
 )
